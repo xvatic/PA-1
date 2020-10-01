6d92004b0e77946ad31bd1d9346dc975eb38e947
xvatic <glazkovE@gmail.com>
2020-10-01 23:48:24 +0300

    Ukol2 fixed

--- Cviceni-2/Ukol2/Ukol2/main.c
+++ Cviceni-2/Ukol2/Ukol2/main.c
@@ -7,18 +7,44 @@
 //
 
 #include <stdio.h>
+#include <math.h>
 #include <string.h>
+
+int convert(char * binary, int n) {
+    int val=0;
+    
+    if (binary[0] == '0') {
+        for(int i=n-1; i>0; i--) {
+            val+=(int)(binary[i]-'0')*pow(2, n-i-1);
+        }
+        
+    } else if(binary[0] == '1') {
+        for(int i=1; i<=10; i++) {
+            if(binary[i] == '0') binary[i]='1';
+            else if(binary[i] == '1') binary[i]='0';
+        }
+        for(int i=n-1; i>=1; i--) {
+            val+=(int)(binary[i]-'0')*pow(2, n-i-1);
+        }
+        val++;
+        val = -val;
+    } else {printf("wrong format");}
+        
+    return val;
+}
 
 
 int main(int argc, const char * argv[]) {
     int n = 0;
     scanf("%d\n", &n);
     
-    char x[n];
-    char y[n];
-    fgets(x, sizeof(x), stdin);
-    fgets(y, sizeof(y), stdin);
-    printf("gdgfdgd");
+    char x[n+1];
+    char y[n+1];
+    fgets(x,sizeof(x), stdin);
+    fgets(y,sizeof(y), stdin);
+    
+    printf("%d\n%d\n", convert(x, n), convert(y, n));
+    
     
 
     return 0;

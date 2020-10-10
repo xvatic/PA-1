be2f8925ff9be32557818f7b1b5d152e8480f666
xvatic <glazkovE@gmail.com>
2020-10-10 13:36:14 +0300

    pr final

---
+++ Cviceni -3/Délka intervalu/Délka intervalu/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Délka intervalu
+//
+//  Created by Евгений on 10/9/20.
+//  Copyright © 2020 Евгений. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

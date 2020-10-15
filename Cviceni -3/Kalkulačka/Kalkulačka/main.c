a4ee1e6018cfe3e54f6787e3a08858b5023d8c11
xvatic <glazkovE@gmail.com>
2020-10-15 10:57:34 +0200

    4f

--- Cviceni -3/Kalkulačka/Kalkulačka/main.c
+++ Cviceni -3/Kalkulačka/Kalkulačka/main.c
@@ -7,11 +7,14 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
 int main(int argc, const char * argv[]) {
     double a,b ;
     double c = 0.0;
     char op, r;
+
     printf("Zadejte vzorec:\n");
     if (scanf(" %lf %c %lf %c", &a, &op, &b, &r) != 4 || (op != '+' && op != '-' && op != '*'&& op != '/') || r != '=') {
         printf("Nespravny vstup.\n");
@@ -30,13 +33,27 @@ int main(int argc, const char * argv[]) {
             c = a*b;
             break;
         case '/':
+            if (b == 0) {
+                printf("Nespravny vstup.\n");
+                return 1;
+            }
             c = a/b;
             break;
     }
+    
     if (floor(c) == c ){
+        if (c > 10000000 || c < -10000000) {
+            printf("%g\n",(c));
+        } else {
         printf("%d\n",(int) (floor(c)));
+        }
     } else{
-        printf("%lf\n", c);
+        if(op == '/'){
+            printf("%d\n",(int) trunc(c));
+        } else {
+            printf("%g\n",c);
+        }
+       
     }
 
     

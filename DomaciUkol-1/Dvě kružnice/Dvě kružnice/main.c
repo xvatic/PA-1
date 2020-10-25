27ed7fece819ac23cdbab47d6fb2dc9888f02bda
xvatic <glazkovE@gmail.com>
2020-10-25 20:04:22 +0100

    DU1 final

--- DomaciUkol-1/Dvě kružnice/Dvě kružnice/main.c
+++ DomaciUkol-1/Dvě kružnice/Dvě kružnice/main.c
@@ -26,10 +26,8 @@ void InitKruznice (TKruznice* kruznice, double x, double y, double r) {
 int main(int argc, const char * argv[]) {
     TKruznice k1,k2;
     double x, y, r;
-    long double d, d1, d2, f1, f2;
-    short int xx = -0x5db3; /* !!! short */
-
-    printf ( "%x", xx );
+    long double d,   f1, f2;
+    
 
     printf("Zadejte parametry kruznice #1:\n");
     if (scanf("%lf %lf %lf", &x, &y, &r) != 3 || r <= 0) {
@@ -46,57 +44,51 @@ int main(int argc, const char * argv[]) {
     
     
     d = sqrt( pow( k1.x - k2.x, 2 ) + pow( k1.y - k2.y, 2 ));
-    d1 = (pow(k1.r, 2) - pow(k2.r, 2) + pow(d, 2))/(2*d);
-    d2 = d-d1;
+    
+    
     f1 = 2 * acos((pow(k1.r, 2) - pow(k2.r, 2) + pow(d, 2) )/(2*k1.r*d));
     f2 = 2 * acos((pow(k2.r, 2) - pow(k1.r, 2) + pow(d, 2) )/(2*k2.r*d));
+    if (k1.x == k2.x && k1.y == k2.y && k1.r == k2.r) {
+           long double s = M_PI * pow(k2.r, 2);
+        printf("Kruznice splyvaji, prekryv: %.6Lf\n", s);
+           return 0;
+    }
     
-    if (d == (k1.r+k2.r)) {
+   
+    
+    if (d < fabs(k1.r-k2.r) || fabsl (d-k1.r+k2.r) < 1000 * DBL_EPSILON * fabs (k1.r-k2.r) || fabsl (d+k1.r-k2.r) < 1000 * DBL_EPSILON * fabs (k1.r-k2.r)) {
+        if (k1.r > k2.r) {
+            long double s = M_PI * pow(k2.r, 2);
+            if (fabsl (d-k1.r+k2.r) < 1000 * DBL_EPSILON * fabs (k1.r-k2.r)) {
+                printf("Vnitrni dotyk, kruznice #2 lezi uvnitr kruznice #1, prekryv: %.6Lf\n", s);
+                return 0;
+            }
+            printf("Kruznice #2 lezi uvnitr kruznice #1, prekryv: %.6Lf\n", s);
+            return 0;
+        } else {
+            long double s = M_PI * pow(k1.r, 2);
+            
+            if (fabsl (d+k1.r-k2.r) < 1000 * DBL_EPSILON * fabs (k1.r-k2.r)) {
+                printf("Vnitrni dotyk, kruznice #1 lezi uvnitr kruznice #2, prekryv: %.6Lf\n", s);
+                return 0;
+            }
+            printf("Kruznice #1 lezi uvnitr kruznice #2, prekryv: %.6Lf\n", s);
+            return 0;
+        }
+    }
+    if (fabsl (d-(k1.r+k2.r)) < 1000 * DBL_EPSILON * fabs (k1.r+k2.r)) {
         printf("Vnejsi dotyk, zadny prekryv.\n");
         return 0;
     }
-    
-    
     if (d > k1.r+k2.r) {
         printf("Kruznice lezi vne sebe, zadny prekryv.\n");
         return 0;
     }
-    if (k1.x == k2.x && k1.y == k2.y && k1.r == k2.r) {
-        float s = M_PI * pow(k2.r, 2);
-        printf("Kruznice splyvaji, prekryv: %.6f\n", s);
-        return 0;
-    }
-    double k = floor(d * 10000000000000)/10000000000000;
-    double kr = floor(fabs(k1.r-k2.r) * 10000000000000)/ 10000000000000;
-    
-    if (d <= fabs(k1.r-k2.r)) {
-        if (k1.r > k2.r) {
-            float s = M_PI * pow(k2.r, 2);
-            if (roundf(d * 10000000000000)/10000000000000 == roundf(fabs(k1.r-k2.r) * 10000000000000)/ 10000000000000) {
-                printf("Vnitrni dotyk, kruznice #2 lezi uvnitr kruznice #1, prekryv: %.6f\n", s);
-                return 0;
-            }
-            printf("Kruznice #2 lezi uvnitr kruznice #1, prekryv: %.6f\n", s);
-            return 0;
-        } else {
-            float s = M_PI * pow(k1.r, 2);
-            
-            if (roundf(d * 10000000000)/10000000000 == roundf(fabs(k1.r-k2.r) * 10000000000)/ 10000000000) {
-                printf("Vnitrni dotyk, kruznice #1 lezi uvnitr kruznice #2, prekryv: %.6f\n", s);
-                return 0;
-            }
-            printf("Kruznice #1 lezi uvnitr kruznice #2, prekryv: %.6f\n", s);
-            return 0;
-        }
-    }
-    if (roundf(d * 10000000000)/10000000000 == roundf((k1.r+k2.r) * 10000000000)/ 10000000000) {
-        printf("Vnejsi dotyk, zadny prekryv.\n");
-        return 0;
-    }
+   
     
 
-    float s2 = (pow(k1.r, 2) * (f1 - sin(f1)))/2 + (pow(k2.r, 2) * (f2 - sin(f2)))/2 ;
-    printf("Kruznice se protinaji, prekryv: %.6f\n", s2);
+    long double s2 = (pow(k1.r, 2) * (f1 - sin(f1)))/2 + (pow(k2.r, 2) * (f2 - sin(f2)))/2 ;
+    printf("Kruznice se protinaji, prekryv: %.6Lf\n", s2);
     
     return 0;
 }

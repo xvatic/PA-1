5e4d9a1138cb5f8c9018e54b9bfd126c2ab08c9e
xvatic <glazkovE@gmail.com>
2020-10-31 02:01:10 +0100

    du2 pre-final

--- DomaciUkol-2/Perfektní mocnina/Perfektní mocnina/main.c
+++ DomaciUkol-2/Perfektní mocnina/Perfektní mocnina/main.c
@@ -7,6 +7,8 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
 
 long long countn(long long a, long long b, long long pp[]) {
     int count = 0;
@@ -15,8 +17,8 @@ long long countn(long long a, long long b, long long pp[]) {
         if(k==0){
             continue;
         }
-        int first = 4;
-        int last = 33000;
+        int first = 0;
+        int last = 46381;
         int middle = (first+last)/2;
         while (first <= last) {
            if (pp[middle] < k)
@@ -33,38 +35,44 @@ long long countn(long long a, long long b, long long pp[]) {
     return count;
 }
 
-void swap(long long* xp, long long* yp)
-{
-    long long temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
-  
-// Function to perform Selection Sort
-void selectionSort(long long arr[], long long n)
-{
-    long long i, j, min_idx;
-  
-    // One by one move boundary of unsorted subarray
-    for (i = 0; i < n - 1; i++) {
-  
-        // Find the minimum element in unsorted array
-        min_idx = i;
-        for (j = i + 1; j < n; j++)
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
-  
-        // Swap the found minimum element
-        // with the first element
-        swap(&arr[min_idx], &arr[i]);
-    }
+void quicksort(long long number[],long long first,long long last){
+   long long i, j, pivot, temp;
+
+   if(first<last){
+      pivot=first;
+      i=first;
+      j=last;
+
+      while(i<j){
+         while(number[i]<=number[pivot]&&i<last)
+            i++;
+         while(number[j]>number[pivot])
+            j--;
+         if(i<j){
+            temp=number[i];
+            number[i]=number[j];
+            number[j]=temp;
+         }
+      }
+
+      temp=number[pivot];
+      number[pivot]=number[j];
+      number[j]=temp;
+      quicksort(number,first,j-1);
+      quicksort(number,j+1,last);
+      
+
+   }
 }
 
+
+
 int main(int argc, const char * argv[]) {
-    long long a,b;
-    long long pp[33000-13];
-    int count = 0;
-    long long n = 1000000000;
+    long long a,b = -1;
+    long long pp[46382];
+    pp[0] = 1;
+    int count = 1;
+    long long n = 2000000000;
     for (long long i = 2; i * i <= n; i++) {
         long long j = i * i;
         pp[count]=j;
@@ -75,15 +83,17 @@ int main(int argc, const char * argv[]) {
             j = j * i;
         }
     }
-    long long np = sizeof(pp) / sizeof(pp[0]);
-    selectionSort(pp, np);
     
+    quicksort(pp, 0, 46381);
+    int result;
     printf("Intervaly:\n");
-    while (scanf("%lld %lld", &a, &b) ) {
-        if (a>b || a<0 || b<0) {
+    while ( (result = (scanf("%lld %lld", &a, &b))) != EOF  || (result = (scanf("%lld", &a))) != EOF) {
+        
+        if ((a>b && b!=-1) || a<0 || b<0  || result != 2 ) {
             printf("Nespravny vstup.\n");
             return 1;
         }
+        
         printf("<%lld;%lld> -> %lld\n", a,b, b-a+1 -countn(a,b, pp));
     }
     return 0;

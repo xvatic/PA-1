ff77de12a2fa077c330f89c7a39e548c1373902e
xvatic <glazkovE@gmail.com>
2020-12-12 17:15:26 +0100

    du7 init

---
+++ DomaciUkol-7/Žetony/Žetony/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+int rec ( int x, int y ,int[] m)
+{
+  
+}
+
+int cascade (int l, int r, int type, ) {
+    
+}
+
+int main ( void )
+{
+    
+    
+    int r = rec ( 167, 15 );
+    
+    return 0;
+}
